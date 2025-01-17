349aac7c196c2573af09e80cb9c0b80e9dd36b0e
Vikas TU <vikastuvikki@gmail.com>
2025-01-17 14:08:32 +0530

    logo added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import logo from './../../public/logo.webp'
+import logo from './../../public/Orange-White-Logo.png'
 
 const Header = () => {
   return (
-    <header className='bg-slate-200 lg:bg-blue px-6 p-4 sticky top-0 z-10' >
-        <img src={logo} alt="" className='w-32' />
+    <header className='bg-blue px-6 p-4 sticky top-0 z-10' >
+        <img src={logo} alt="" className='w-24' />
     </header>
   )
 }
